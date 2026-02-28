aa08250873ccf377ebc8fc9ff325c470953cae67
mark_codez <mwangimark900@gmail.com>
2026-02-28 03:10:49 +0000

    fix deploy

--- afyabora/vite.config.js
+++ afyabora/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: "0.0.0.0",
+    port: process.env.PORT ? Number(process.env.PORT) : 10000,
+    allowedHosts: [".onrender.com"]
+  }
 })
